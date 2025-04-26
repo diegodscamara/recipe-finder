22127eaf4d5ac0b76577d85e8035a3190be2cfb4
Diego Câmara <diegodscamara@gmail.com>
2025-04-25 20:53:47 -0400

    Remove integration tests for the <App /> component as part of the testing cleanup process.

--- tests/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import App from '../src/App';
-
-describe('<App /> Integration Tests', () => {
-  it('renders the main application title and components', () => {
-    render(<App />);
-  });
-});
